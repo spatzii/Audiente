0d844180b43f3b49e40164f68b27a60281f39701
spacedk <pana.stefan@gmail.com>
2023-02-05 15:26:50 +0200

    Move pandas.style output to backend & add color coding to rating averages

--- Home.py
+++ Home.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pathlib
-import datetime
+import matplotlib
 import main
+import datetime
+import pandas as pd
 
 
 st.title("Audiențe Digi24")
@@ -12,27 +14,25 @@ files = (list(pathlib.Path('Data/').glob(selection.strftime('%Y') + '/' +
                                          selection.strftime('%d') + '/' +
                                          '*.csv')))
 
+
 with st.expander("Audiențe whole day"):
     for file in files:
         st.write("Acestea sunt audiențele din ", selection.strftime('%x'))
-        rating_file = main.test_print(file).style.set_precision(2)  # .highlight_max(axis=0)
-        st.dataframe(rating_file)
+        rating_file = main.whole_day(file)
+        st.dataframe(rating_file, width=400)
 
 
 with st.expander("Audiențe tronsoane"):
     for file in files:
-        time_slots = st.selectbox('Selectează tronsonul', main.tronsoane, key="tronson", label_visibility="hidden")
+        time_slots = st.selectbox('Selectează tronsonul', main.tronsoane, key="tronson",
+                                  label_visibility="hidden")
         if time_slots is not main.tronsoane[0]:
-            new_rating_file = main.audienta_tronsoane(file, time_slots).style.set_precision(2)
-            st.dataframe(new_rating_file)
+            new_rating_file = main.audienta_tronsoane(file, time_slots)
+            st.dataframe(new_rating_file, width=600)
 
     # if time_slots is not None:
     #     new_rating_file = main.audienta_tronsoane(rating_file, time_slots)
     #     st.dataframe(new_rating_file)
-
-
-
-
 
 
 # st.session_state['tronson']
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pathlib
+
 # import pandas.io.formats.style
 
 tronsoane = ["Selectează tronsonul", '2:00 - 6:00', '6-9 Matinal', '9-12 Știrile Dimineții',
@@ -32,46 +33,42 @@ def read_audiente(file, filename):
         return False
 
 
-def test_print(file):
-    csv = pd.read_csv(file)
-    # pandas.io.formats.style.Styler.format(csv, precision=2)
+def whole_day(file):
+    csv = pd.read_csv(file).iloc[:, 1:4]
+    hl_averages = [16, 29, 42, 55, 60, 73, 78, 91, 100]  # Indexes of rating averages in dataframe, for highlighting
+    print(type(csv))
+    csv = csv.style.apply(lambda x: ['color: red' if x.name in hl_averages else '' for i in x], axis=1).set_precision(2)
     return csv
 
 
 def audienta_tronsoane(other_file, time_slots):
+    hl_averages = [16, 29, 42, 55, 60, 73, 78, 91, 100]
     if time_slots == tronsoane[1]:
         new_csv = pd.read_csv(other_file)
-        new_csv = new_csv.iloc[0:17, 0:4]
-        return new_csv
+        new_csv = new_csv.iloc[0:17, 1:4]
     if time_slots == tronsoane[2]:
         new_csv = pd.read_csv(other_file)
-        new_csv = new_csv.iloc[17:30, 0:4]
-        return new_csv
+        new_csv = new_csv.iloc[17:30, 1:4]
     if time_slots == tronsoane[3]:
         new_csv = pd.read_csv(other_file)
-        new_csv = new_csv.iloc[30:43, 0:4]
-        return new_csv
+        new_csv = new_csv.iloc[30:43, 1:4]
     if time_slots == tronsoane[4]:
         new_csv = pd.read_csv(other_file)
-        new_csv = new_csv.iloc[43:56, 0:4]
-        return new_csv
+        new_csv = new_csv.iloc[43:56, 1:4]
     if time_slots == tronsoane[5]:
         new_csv = pd.read_csv(other_file)
-        new_csv = new_csv.iloc[56:61, 0:4]
-        return new_csv
+        new_csv = new_csv.iloc[56:61, 1:4]
     if time_slots == tronsoane[6]:
         new_csv = pd.read_csv(other_file)
-        new_csv = new_csv.iloc[61:74, 0:4]
-        return new_csv
+        new_csv = new_csv.iloc[61:74, 1:4]
     if time_slots == tronsoane[7]:
         new_csv = pd.read_csv(other_file)
-        new_csv = new_csv.iloc[74:79, 0:4]
-        return new_csv
+        new_csv = new_csv.iloc[74:79, 1:4]
     if time_slots == tronsoane[8]:
         new_csv = pd.read_csv(other_file)
-        new_csv = new_csv.iloc[79:92, 0:4]
-        return new_csv
+        new_csv = new_csv.iloc[79:92, 1:4]
     if time_slots == tronsoane[9]:
         new_csv = pd.read_csv(other_file)
-        new_csv = new_csv.iloc[92:106, 0:4]
-        return new_csv
+        new_csv = new_csv.iloc[92:106, 1:4]
+    return new_csv.style.apply(lambda x: ['color: red' if x.name in hl_averages else '' for i in x],
+                               axis=1).set_precision(2)
