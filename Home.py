f0cc45ca02bf5f0899c0c601670e329c0fab8211
spacedk <pana.stefan@gmail.com>
2023-02-07 07:57:41 +0200

    Add upload for ratings by minute, generate slot graph by minute

--- Home.py
+++ Home.py
@@ -1,44 +1,47 @@
 import streamlit as st
 import pathlib
 import data
+import libraries
 import datetime
-
 
 st.title("Audiențe Digi24")
 
 selection = st.date_input('Selectează data audiențelor...', key='date_select')
-files = (list(pathlib.Path('Data/').glob(selection.strftime('%Y') + '/' +
-                                         selection.strftime('%m') + '/' +
-                                         selection.strftime('%d') + '/' +
-                                         '*.csv')))
-
+quarter_files = (list(pathlib.Path('Data/Quarters').glob(selection.strftime('%Y') + '/' +
+                                                         selection.strftime('%m') + '/' +
+                                                         selection.strftime('%d') + '/' +
+                                                         '*.csv')))
+minute_files = (list(pathlib.Path('Data/Minutes').glob(selection.strftime('%Y') + '/' +
+                                                       selection.strftime('%m') + '/' +
+                                                       selection.strftime('%d') + '/' +
+                                                       '*.csv')))
 
 with st.expander("Audiențe whole day"):
-    for file in files:
+    for file in quarter_files:
         st.write("Acestea sunt audiențele din ", selection.strftime('%x'))
-        rating_file = data.whole_day(file)
-        st.dataframe(rating_file, width=400)
-        daily_chart_btn = st.button('Rapoarte whole day', key=['daily_chart'])
-        if daily_chart_btn is True:
-            chart_rating_file = data.whole_day(file, chart=True)
-            st.line_chart(chart_rating_file, x="Timebands", y=["Digi 24", "Antena 3 CNN"])
-
+        rating_file = data.whole_day_ratings(file)
+        st.dataframe(rating_file, width=400, height=600)
+        with st.sidebar:
+            daily_chart_btn = st.button('Rapoarte whole day', key=['daily_chart'])
+            if daily_chart_btn is True:
+                chart_rating_file = data.whole_day_ratings(file, chart=True)
+                st.line_chart(chart_rating_file, x="Timebands", y=["Digi 24", "Antena 3 CNN"])
 
 with st.expander("Audiențe tronsoane"):
-    for file in files:
-        time_slots = st.selectbox('Selectează tronsonul', data.tronsoane, key="tronson", label_visibility="hidden")
-        if time_slots is not data.tronsoane[0]:
-            new_rating_file = data.audienta_tronsoane(file, time_slots)
-            st.dataframe(new_rating_file, width=600)
+    for file in quarter_files:
+        time_slots = st.selectbox('Selectează tronsonul', libraries.digi24_slot_names,
+                                  key="tronson", label_visibility="hidden")
+        try:  # if time_slots is not libraries.digi24_slots[0]:
+            new_rating_file = data.slot_ratings(file, time_slots)
+            st.dataframe(new_rating_file, width=600, height=500)
+        except ValueError:
+            pass
+        with st.sidebar:
+            hourly_chart_btn = st.button('Rapoarte de tronson', key=['hourly_chart'])
 
-        hourly_chart_btn = st.button('Rapoarte de tronson', key=['hourly_chart'])
-
-        if hourly_chart_btn is True:
-            rating_file = data.audienta_tronsoane_for_graph(file, time_slots)
-            st.line_chart(rating_file, x="Timebands", y=["Digi 24", "Antena 3 CNN"])
-
+            if hourly_chart_btn is True:
+                for minute_file in minute_files:
+                    minute_file = data.slot_ratings_for_graph_by_minute(minute_file, time_slots)
+                    st.line_chart(minute_file, x="Timebands", y=["Digi 24", "Antena 3 CNN"], width=800)
 
 # st.session_state
-
-
-
