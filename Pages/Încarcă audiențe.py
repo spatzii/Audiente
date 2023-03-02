2bcad156364b9c783d7593e3cc2a0a6af4fe7a70
stefanpana <pana.stefan@gmail.com>
2023-03-02 11:26:57 +0200

    Upload rating files, convert to one CSV file Quarters+Minutes but cleaned

--- Pages/Încarcă audiențe.py
+++ Pages/Încarcă audiențe.py
@@ -4,16 +4,7 @@ import classes as cls
 
 try:
     my_file = st.file_uploader('Alege fisierele...', type=['xlsx', 'zip'], key='upload', accept_multiple_files=True)
+    cls.CSVWriter(my_file[0], my_file[1]).create_csv()
 
-    for file in my_file:
-        cls.CSVWriter(file, file.name).check_date()
-        # data.xlsx_to_csv(file, file.name)
-
-    if len(my_file) > 1:
-        st.success("Fișierele au fost urcate cu succes!")
-    if len(my_file) == 1:
-        st.success("Fișierul a fost urcate cu succes!")
-
-
-except AttributeError:
+except IndexError:
     pass
