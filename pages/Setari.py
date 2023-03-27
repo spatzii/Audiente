546d62e7745bfbe4ccb7a16576d39d9ed5587c91
spacedk <pana.stefan@gmail.com>
2023-03-27 20:05:13 +0300

    Change settings to Class settings

--- pages/Setari.py
+++ pages/Setari.py
@@ -1,15 +1,16 @@
 import streamlit as st
-import db_factory
+from db_factory import EmailSettings
 
 
 def change_email():
     email = st.session_state.set_email
-    db_factory.clear_email()
-    db_factory.insert_email(email)
+    mail_settings = EmailSettings()
+    mail_settings.clear_email()
+    mail_settings.insert_email(email)
 
 
 def return_email():
-    return db_factory.read_table()
+    return EmailSettings.read_table()
 
 
 def email_form():
@@ -18,6 +19,9 @@ def email_form():
             with st.form(key='email_form', clear_on_submit=True):
                 email = st.text_input('Introduce adresa', key='set_email')
                 submit = st.form_submit_button('Submit', on_click=change_email)
+        if st.button('Verifică adresa actuală'):
+            mail_settings = EmailSettings()
+            st.write(mail_settings.read_table())
 
 
 email_form()
