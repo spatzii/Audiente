0460850bfba46488ec766001f8a7098ff87da3f9
spacedk <pana.stefan@gmail.com>
2023-03-27 21:48:51 +0300

    Add slot selection in Settings (dummy code)

--- db_factory.py
+++ db_factory.py
@@ -2,29 +2,38 @@ import sqlite3 as db
 
 
 class EmailSettings:
+    """Creates ONE email id with selected slot for sending email information. Replaced for every address added"""
+
     def __init__(self):
         self.conn = db.connect('settings.db')
         self.conn.row_factory = lambda crs, row: row[0]
         self.cursor = self.conn.cursor()
+        self.create_table()
 
     def create_table(self):
-        self.cursor.execute('CREATE TABLE IF NOT EXISTS email_settings(receiver)')
+        return self.cursor.execute('CREATE TABLE IF NOT EXISTS email_settings(id INT, receiver, slot)')
 
     def read_table(self):
         res = self.cursor.execute('SELECT receiver FROM email_settings')
         return res.fetchone()
 
-    def insert_email(self, email):
-        self.cursor.execute(f"""
-                INSERT INTO email_settings VALUES
-                ('{email}')
-        """)
+    def update_email(self, email):
+        self.cursor.execute(f"""UPDATE email_settings SET receiver = '{email}' WHERE id=1""")
         self.conn.commit()
         self.conn.close()
 
-    def clear_email(self):
-        self.cursor.execute("""DROP TABLE IF EXISTS email_settings""")
-        self.create_table()
+    def update_slot(self, slot):
+        self.cursor.execute(f"""UPDATE email_settings SET slot = '{slot}' WHERE id=1""")
+        self.conn.commit()
+        self.conn.close()
+
+    def read_slot(self):
+        res = self.cursor.execute('SELECT slot FROM email_settings')
+        return res.fetchone()
+
+
+
+
 
 
 
--- pages/Setari.py
+++ pages/Setari.py
@@ -1,27 +1,43 @@
 import streamlit as st
 from db_factory import EmailSettings
+from classes import DayOperations
 
 
 def change_email():
     email = st.session_state.set_email
     mail_settings = EmailSettings()
-    mail_settings.clear_email()
-    mail_settings.insert_email(email)
+    mail_settings.update_email(email)
+
+
+def change_slot():
+    slot = st.session_state.set_slot
+    slot_settings = EmailSettings()
+    slot_settings.update_slot(slot)
 
 
 def return_email():
-    return EmailSettings.read_table()
+    mail_settings = EmailSettings()
+    mail_settings.read_table()
 
 
 def email_form():
     with st.expander('Setări email'):
         if st.button('Alege destinatarul'):
             with st.form(key='email_form', clear_on_submit=True):
-                email = st.text_input('Introduce adresa', key='set_email')
-                submit = st.form_submit_button('Submit', on_click=change_email)
+                st.text_input('Introduce adresa', key='set_email')
+                st.form_submit_button('Submit', on_click=change_email)
         if st.button('Verifică adresa actuală'):
             mail_settings = EmailSettings()
             st.write(mail_settings.read_table())
+
+        if st.button("Alege tronsonul"):
+            with st.form(key='slot_form', clear_on_submit=True):
+                st.selectbox('Tronsoane', options=['16-18 Știrile Zilei', '12-14 Știrile Amiezii'], key='set_slot')
+                st.form_submit_button('Submit', on_click=change_slot)
+
+        if st.button('Verifică tronsonul actual'):
+            slot_settings = EmailSettings()
+            st.write(slot_settings.read_slot())
 
 
 email_form()
